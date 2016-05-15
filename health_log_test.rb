df123566863412970e0fa2abe2ad9268f60cdd85
John Dowd <dowdjj@gmail.com>
2016-05-15 07:58:02 -0700

    refactors tests

--- health_log_test.rb
+++ health_log_test.rb
@@ -17,67 +17,25 @@ class HealthLogTest < MiniTest::Test
   end
 
   def test_adding_an_entry
-    input = <<-TXT.gsub(/\s+/,' ').strip << ("\n")
-      200.5 dq: ate cookie dv: ate too much for lunch s: not bad x: strong workout
-    TXT
-    @log.entry input
-    expected = {
-      @date => {
-        weight: 200.5,
-        diet: {
-          quality: "ate cookie",
-          volume: "ate too much for lunch"
-        },
-        sleep: "not bad",
-        exercise: "strong workout",
-      }
-    }.to_yaml
+    @log.entry sample_input
     actual = File.read @file
-    assert_equal expected, actual
+    assert_equal expected_output.to_yaml, actual
   end
 
   def test_add_partial_data_to_a_date_entry
-    input = <<-TXT.gsub(/\s+/,' ').strip << ("\n")
-      200.5 dq: ate cookie dv: ate too much bl: 4 s: not bad x: strong workout
-    TXT
-    @log.entry input
+    @log.entry sample_input
     @log.entry "x: partial entry"
-    expected = {
-      @date => {
-        weight: 200.5,
-        diet: {
-          quality: "ate cookie",
-          volume: "ate too much"
-        },
-        belt_loop: '4',
-        sleep: "not bad",
-        exercise: "partial entry",
-      }
-    }.to_yaml
+    expected = expected_output
+    expected[@date][:exercise] = "partial entry"
     actual = File.read @file
-    assert_equal expected, actual
+    assert_equal expected.to_yaml, actual
   end
 
-  def test_add_partial_data_to_a_date_entry
-    input = <<-TXT.gsub(/\s+/,' ').strip << ("\n")
-      200.5 dq: ate cookie dv: ate too much bl: 4 s: not bad x: strong workout
-    TXT
-    @log.entry input
+  def test_handles_empty_input
+    @log.entry sample_input
     @log.entry "\n"
-    expected = {
-      @date => {
-        weight: 200.5,
-        diet: {
-          quality: "ate cookie",
-          volume: "ate too much"
-        },
-        belt_loop: '4',
-        sleep: "not bad",
-        exercise: "strong workout",
-      }
-    }.to_yaml
     actual = File.read @file
-    assert_equal expected, actual
+    assert_equal expected_output.to_yaml, actual
   end
 
   def test_skipping_weight
@@ -85,23 +43,10 @@ class HealthLogTest < MiniTest::Test
       dq: ate cookie dv: ate too much bl: 4 s: not bad x: strong workout
     TXT
     @log.entry input
-    @log.entry "x: partial entry"
-    expected = {
-      @date => {
-        diet: {
-          quality: "ate cookie",
-          volume: "ate too much"
-        },
-        belt_loop: '4',
-        sleep: "not bad",
-        exercise: "partial entry",
-      }
-    }.to_yaml
+    expected = expected_output
+    expected[@date].delete(:weight)
     actual = File.read @file
-    assert_equal expected, actual
-  end
-
-  def test_handles_empty_input
+    assert_equal expected.to_yaml, actual
   end
 
   def test_reviewing_all_entries
@@ -114,6 +59,27 @@ class HealthLogTest < MiniTest::Test
   end
 
   def test_report_for_last_n_days
+  end
+
+  def sample_input
+    <<-TXT.gsub(/\s+/,' ').strip << ("\n")
+      200.5 dq: ate cookie bl: 4 dv: ate too much s: not bad x: strong workout
+    TXT
+  end
+
+  def expected_output
+    {
+      @date => {
+        weight: 200.5,
+        diet: {
+          quality: "ate cookie",
+          volume: "ate too much"
+        },
+        belt_loop: '4',
+        sleep: "not bad",
+        exercise: "strong workout",
+      }
+    }
   end
 
 end
